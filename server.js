32d2583b96291d5d1f1d9827a0d2ec63bdb64feb
Samuel Popoola <samuelpopoola323@gmail.com>
2025-10-18 00:56:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,10 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 // ====== MongoDB Connection ======
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(MONGO_URI);
 .then(() => console.log("✅ MongoDB connected"))
 .catch(err => console.error("❌ MongoDB error:", err));
 
